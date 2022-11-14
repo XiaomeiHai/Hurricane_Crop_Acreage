f35a5bd5c134dc144ec792386ad4a0641a6c65e2
RWorkshop-at-UF <91082253+RWorkshop-at-UF@users.noreply.github.com>
2022-11-14 14:19:06 -0500

    A quick tutorial on how to open shape file in R

---
+++ Code/R/Open_shape_file_in_R.R
@@ -0,0 +1,25 @@
+# A quick guide on how to open shape file in R
+
+library(dplyr)
+library(sf)
+
+# setwd("tek/TeamAccenture") # Change your path
+
+# Original Shape data source: https://www2.census.gov/geo/tiger/GENZ2018/shp/
+df = sf::st_read("../Data/cb_2018_us_county_500k.shp")
+# Check the columns and their type
+str(df)
+
+# Note you are only interested in FL. 
+# So, filter by state code. 
+# Look up State code for FL... 
+# https://www.census.gov/library/reference/code-lists/ansi.html#state
+# https://www.census.gov/geographies/reference-files.html
+# State code for FL: 12
+df %>%
+  dplyr::filter(STATEFP == "12") -> dff # dff as in Dataframe Florida
+
+# You now have polygons for plotting counties in FL
+plot(dff)
+
+# Use the State and County code as a key fields to merge with other files
